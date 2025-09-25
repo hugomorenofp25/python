043d1bf02ae805dc8f4b22630864c3ec42b58701
Hugo Moreno Expósito <hugo.moreno.dev@gmail.com>
2025-09-25 15:50:16 +0200

    a mitad de variables

---
+++ python/0_introduccion.py
@@ -0,0 +1,20 @@
+
+#! Esto es un comentatio Hola mundo
+print ("Hola Mundo")
+print ('Hola Mundo')
+
+"""
+Este es un comentario en varias líneas
+"""
+
+'''
+Este también es un comentario en varias líneas
+'''
+
+#! Consultar el tipo de dato que mostramos
+print (type ("Hola")) # Devuelve el tipo del contenido en este caso --> Tipo 'str'
+print (type (5)) # Devuelve el tipo del contenido en este caso --> 'int'
+print (type (1.5)) # Devuelve el tipo del contenido en este caso --> 'float'
+print (type (True)) # Devuelve el tipo del contenido en este caso --> 'bool'
+
+#! min 47:45
---
+++ python/1_variables.py
@@ -0,0 +1,22 @@
+
+#! TIPOS DE VARIABLES
+
+'''
+Las variables siempre han de ser en minúsculas y separadas por _
+'''
+# Esto es una variable string --> de texto
+my_variable = "Mi variable String"
+print(my_variable) 
+
+# Esto es una variable int --> de números
+my_int = 5
+print(my_int)
+
+# Esto es una variable bool --> un booleano (True o False)
+my_bool = True
+print(my_bool)
+
+# Aquí estamos imprimiendo "Mi variable String 5 True
+print(my_variable, my_int, my_bool)
+
+#! MIN 1:00:15 DE VÍDEO
