e7dfc7c34ee29a1de6f27f516393bedf7c2a4c4e
Hugo Moreno <hugomorenofp25@ibf.cat>
2025-10-08 18:06:06 +0200

    corregido ejercicio 1 piedra-pap-tij

--- clase python/Juegos_Hugo/piedra_papel_tijera.py
+++ clase python/Juegos_Hugo/piedra_papel_tijera.py
@@ -14,23 +14,23 @@ if selecciona_juego == 1:
     print("Tienes que adivinar el número aleatorio entre el 1 y el 10")
     print("Tienes 3 intentos, si fallas los 3 pierdes")
     number = random.randint(1,10)
-    # print("Número random es: ", number) #comentar esto al acabar
+    #print("Número random es: ", number) #comentar esto al acabar
 
     first_try = int(input("Dime el primer número: "))
 
     if first_try > number:
-        print("Es mas grande del que buscamos")
-    else:
         print("Es mas pequeño del que buscamos")
+    else:
+        print("Es mas grande del que buscamos")
     if first_try == number:
         print("Felicidades, el numero era: ", number, "has GANADO")
     else:
         print("No es el número: ", first_try)
         second_try = int(input("Dime el segundo número: "))
         if second_try > number:
-            print("Es mas grande del que buscamos")
-        else:
             print("Es mas pequeño del que buscamos")
+        else:
+            print("Es mas grande del que buscamos")
         if second_try == number:
             print("Felicidades, el numero era: ", number, "has GANADO")
         else:
